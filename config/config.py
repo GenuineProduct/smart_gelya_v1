44a89dab76fdc74f4c04a173db715eed78e79c5f
GenuineProduct <gleb.mahora.2002@gmail.com>
2025-12-01 21:03:09 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN ='8024175589:AAEKqz9ufuXbk76_Rs9M9V2piCgM_3eJxko'
+BOT_TOKEN ='your token'
 PLAYLISTS_PATH = "треки"  # Базовая папка где лежат все плейлисты
 print(f"DEBUG: BOT_TOKEN loaded: {BOT_TOKEN is not None}")
 
